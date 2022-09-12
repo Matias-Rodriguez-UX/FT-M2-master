a47d2595d97dfb163b0deb1fb32882901d6e14f5
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2022-09-12 17:51:52 -0300

    Tarea ES6 lista

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -4,13 +4,20 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   if (typeof startEl === "undefined") {
     startEl = document.body;
   }
+  
 
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
-  // usa matchFunc para identificar elementos que matchien
-
+  // usa matchFunc para erteridentificar elementos que matchien
   // TU CÓDIGO AQUÍ
-  
-};
+  if (matchFunc(startEl)){
+  resultSet.push(startEl)
+  }
+  for (const ele of startEl.children) {
+    let elementos = traverseDomAndCollectElements(matchFunc, ele)
+    resultSet = [...resultSet, ...elementos]
+  }
+  return resultSet
+  }
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
@@ -18,7 +25,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0]=== "#") return "id";
+  else if(selector[0]=== ".") return "class";
+  else if(selector.includes(".")) return "tag.class";
+  else return "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -29,17 +39,46 @@ var selectorTypeMatcher = function(selector) {
 var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
-  if (selectorType === "id") { 
-   
-  } else if (selectorType === "class") {
-    
-  } else if (selectorType === "tag.class") {
-    
-  } else if (selectorType === "tag") {
-    
-  }
-  return matchFunction;
-};
+    if (selectorType === "id") {
+      matchFunction = function(el){
+        let selid = selector.replace("#", "")
+        let ids = el.id
+        return ids.toLowerCase() === selid.toLowerCase()
+      }
+    } else if (selectorType === "class") {
+      matchFunction = function(el){
+        let selcl = selector.replace(".", "")
+        let clss = el.classList
+        let cont = 0
+        clss.forEach(element => {
+          if(element.toLowerCase()=== selcl.toLowerCase())
+          cont ++
+        });
+        return cont>0;
+      }
+      
+    } else if (selectorType === "tag.class") {
+      matchFunction = function(el){
+        let sel = selector.split(".")
+        let pri = sel[0];
+        let sec = sel[1];
+        let cont = 0
+        if(pri.toLowerCase() === el.tagName.toLowerCase()){
+        let clss = el.classList
+      	clss.forEach(element => {
+        if(element.toLowerCase() === sec.toLowerCase())
+        cont ++
+      	});
+        }
+        return cont>0;
+      }
+    } else if (selectorType === "tag") {
+      matchFunction = function(el){
+        return selector.toLowerCase() === el.tagName.toLowerCase()
+      }
+    }
+    return matchFunction;
+  };
 
 var $ = function(selector) {
   var elements;
@@ -47,3 +86,4 @@ var $ = function(selector) {
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
+
